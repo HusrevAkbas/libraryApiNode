07584f86dfe921f2952f134c30d5e6880645ae72
HusrevAkbas <husrev.akbas@gmail.com>
2024-06-12 22:47:30 +0200

    User login response changed

--- src/DTO/UserLoginResponse.ts
+++ src/DTO/UserLoginResponse.ts
@@ -2,10 +2,12 @@ import { User } from "../entity/User"
 
 export class UserLoginResponse {
     username:string
+    role:string
     id: string
 
     constructor(user:User){
         this.username = user.username
         this.id = user.id
+        this.role = user.role
     }
 }
